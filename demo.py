e15f15215fcdc642423ced808620c9e3cfa07313
chompa111 <gustavo.lach@gmail.com>
2020-01-03 15:20:34 -0300

    vai e volta

--- demo.py
+++ demo.py
@@ -4,6 +4,11 @@ import time
 path  = r'.\chromedriver.exe'
 browser = webdriver.Chrome(executable_path = path)
 browser.get("https://steamcommunity.com/market/search?q=#p2_popular_desc")
-time.sleep(2)
-menu = browser.find_element_by_css_selector("#result_0")
-menu.click()
+time.sleep(3)
+
+for i in range(10):
+	menu = browser.find_element_by_css_selector("#result_"+str(i))
+	menu.click()
+	time.sleep(2)
+	browser.back()
+	time.sleep(2)
