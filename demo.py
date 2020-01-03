0a7503d0c0f1aebdf425f2ff4d8ad80f69b31155
julianvillega9@gmail.com <julianvillega9@gmail.com>
2020-01-03 14:59:35 -0300

    funcionando

--- demo.py
+++ demo.py
@@ -5,5 +5,5 @@ path  = r'.\chromedriver.exe'
 browser = webdriver.Chrome(executable_path = path)
 browser.get("https://steamcommunity.com/market/search?q=#p2_popular_desc")
 time.sleep(2)
-menu = browser.find_element_by_css_selector("#responsive_menu_logo > img")
+menu = browser.find_element_by_css_selector("#result_0")
 menu.click()
