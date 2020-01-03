78d0a3ea139c210a20839fc1578713d595d87480
julianvillega9@gmail.com <julianvillega9@gmail.com>
2020-01-03 14:57:55 -0300

    teste fail

--- demo.py
+++ demo.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import time
 path  = r'.\chromedriver.exe'
 browser = webdriver.Chrome(executable_path = path)
 browser.get("https://steamcommunity.com/market/search?q=#p2_popular_desc")
+time.sleep(2)
+menu = browser.find_element_by_css_selector("#responsive_menu_logo > img")
+menu.click()
