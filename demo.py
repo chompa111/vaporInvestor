b11713d30f6e12953ac4b8289d066a56ae92c73c
chompa111 <gustavo.lach@gmail.com>
2020-01-03 16:29:45 -0300

    maracutaias com regex

--- demo.py
+++ demo.py
@@ -1,11 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import re
 path  = r'.\chromedriver.exe'
 browser = webdriver.Chrome(executable_path = path)
 browser.get("https://steamcommunity.com/market/listings/730/Shattered%20Web%20Case")
 time.sleep(3)
-print(browser.page_source)
+raw_page_string=browser.page_source
+
+raw_price_rex=re.search(r"var line1=\[(\[.*\])*.*\]",raw_page_string)
+raw_price_string=raw_price_rex.group(1)
+
+raw_objects=re.findall(r"\[(.*?)\],",raw_price_string)
+
+for obj in raw_objects:
+	print("os dados são------>"+obj)
+
 
 '''
 for i in range(10):
