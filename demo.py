5a8245ce92631641d1ab171b1c90e79b620919bb
julianvillega9@gmail.com <julianvillega9@gmail.com>
2020-01-03 15:38:22 -0300

    print de page source

--- demo.py
+++ demo.py
@@ -3,12 +3,15 @@ from selenium.webdriver.common.by import By
 import time
 path  = r'.\chromedriver.exe'
 browser = webdriver.Chrome(executable_path = path)
-browser.get("https://steamcommunity.com/market/search?q=#p2_popular_desc")
+browser.get("https://steamcommunity.com/market/listings/730/Shattered%20Web%20Case")
 time.sleep(3)
+print(browser.page_source)
 
+'''
 for i in range(10):
 	menu = browser.find_element_by_css_selector("#result_"+str(i))
 	menu.click()
 	time.sleep(2)
 	browser.back()
 	time.sleep(2)
+'''
